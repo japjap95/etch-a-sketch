903cdbae78b3c09b37fe41043fa1d5159064dfab
Joshua <joshua.hs.harris@gmail.com>
2020-10-16 22:56:24 +0100

    Project almost complete but must overhaul css class logic to ensure that already coloured blocks do not change colour when a new colur is chosen

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
-const app = () => {
-    const initialSize = 16;
+const app = (x) => {
+    const size = 16;
     const container = document.querySelector(".container");
-    container.setAttribute("grid-template-columns", initialSize);
-    container.setAttribute("grid-row-columns", initialSize);
+    const colorController  = document.querySelector(".permahover");
+    const resetButton = document.getElementById("reset").addEventListener("click", () => {
+        let blocksToReset = container.getElementsByClassName("grid-item");
+        for (let i = 0; i < blocksToReset.length; i++) {
+            blocksToReset[i].classList.remove("permahover");
+        }
+    })
 
-    function makeRows(rows, cols) {
-        container.style.setProperty("--grid-rows", rows);
-        container.style.setProperty("--grid-cols", cols);
-        for (let i = 0; i < (rows * cols); i++){
+    const colourButton = document.getElementById("color");
+    colourButton.addEventListener("keydown", function(e) {
+        if (e.keyCode === 13) {
+            changeColor(colourButton.value);
+        }
+    })
+
+    function changeColor(color) {
+        document.documentElement.style.setProperty("--background-colour", color);
+    }
+
+    function switchState(object) {
+        object.classList.add("permahover");
+    }
+
+    function makeRows(size) {
+        container.textContent = "";
+        container.style.setProperty("--grid-rows", size);
+        container.style.setProperty("--grid-cols", size);
+        for (let i = 0; i < (size * size); i++){
             let newDiv = document.createElement("div");
-            newDiv.textContent = (i + 1);
+            newDiv.setAttribute("id", `grid_item_${i}`)
             newDiv.classList.add("grid-item");
+            newDiv.addEventListener("mouseover", () => {
+                switchState(newDiv);
+            })
             container.appendChild(newDiv);
-        
         }
     }
 
-    makeRows(initialSize, initialSize);
+    const newSizeButton = document.getElementById("newSizeButton");
+    newSizeButton.addEventListener("keydown", function(e) {
+        if (e.keyCode ===  13) {
+            if (newSizeButton.value <= 100) {
+                makeRows(newSizeButton.value);
+            } else {
+                alert("Please enter a number between 1-100. Anything over that and the boxes will be too small!");
+            }
+            
+        }
+    }) 
+
+    document.documentElement.style.setProperty("--background-colour", "blue");
+    makeRows(size);
+    
 
 }
 
